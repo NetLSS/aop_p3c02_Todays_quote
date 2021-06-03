b15ee82ce6923b2a81fd46a8c2190a629af80a63
NetLSS <futureprogrammer@naver.com>
2021-06-03 17:32:16 +0900

    Initial commit

---
+++ app/src/main/java/lilcode/aop/p3/c02/todays_quote/MainActivity.kt
@@ -0,0 +1,11 @@
+package lilcode.aop.p3.c02.todays_quote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
