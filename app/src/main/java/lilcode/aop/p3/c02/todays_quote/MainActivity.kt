13903c1076d195a5da26882e064d82a43d634f17
NetLSS <futureprogrammer@naver.com>
2021-06-04 16:43:36 +0900

    Feat: 복습 주석 추가

--- app/src/main/java/lilcode/aop/p3/c02/todays_quote/MainActivity.kt
+++ app/src/main/java/lilcode/aop/p3/c02/todays_quote/MainActivity.kt
@@ -31,8 +31,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+
+        // 뷰페이저 넘기는 방식 설정
         viewPager.setPageTransformer { page, position ->
-            // position 현재 보이는 화면에서 상대적으로 어느 위치에 있는지
+            // position : 현재 보이는 화면에서 상대적으로 어느 위치에 있는지
 
             when {
                 position.absoluteValue >= 1F -> {
@@ -44,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 else -> {
+                    // 절반 이상 넘길 때 부터 급격하게 투명해지도록 설정.
                     page.alpha = 1F - 2 * position.absoluteValue
                 }
             }
@@ -53,6 +56,8 @@ class MainActivity : AppCompatActivity() {
     private fun initData() {
         // remoteConfig 설정
         val remoteConfig = Firebase.remoteConfig
+
+        // 비동기로 설정되게
         remoteConfig.setConfigSettingsAsync(
             remoteConfigSettings {
                 minimumFetchIntervalInSeconds = 0 // 앱을 들어올 때마다 패치 하도록.
@@ -61,10 +66,12 @@ class MainActivity : AppCompatActivity() {
 
         remoteConfig.fetchAndActivate().addOnCompleteListener {
             // 패치 작업이 완료 된 경우
-            progressBar.visibility = View.GONE
+            progressBar.visibility = View.GONE // 로딩창 숨김
+
             if (it.isSuccessful) {
-                val quotes = parseQuotesJson(remoteConfig.getString("quote"))
-                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+                // json 을 파싱하여 배열로 가져옴.
+                val quotes: List<Quote> = parseQuotesJson(remoteConfig.getString("quote"))
+                val isNameRevealed: Boolean = remoteConfig.getBoolean("is_name_revealed")
 
                 displayQuotesPager(quotes, isNameRevealed)
 
@@ -73,28 +80,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
+        // 어뎁터를 생성하여 할
         val adapter = QuotesPagerAdapter(
             quotes,
             isNameRevealed
         )
 
         viewPager.adapter = adapter
-        // 중앙에서 시작
+        // 중앙에서 시작당 하도록 (그래야 좌우 모두 무한 스크롤 가능하기 때문) 결국 끝에 도달하지만 사용자가 그렇게 하는 경우는 드뭄
+        // smoothScroll 의 경우 부드러운 스크롤인데 중앙으로 그냥 한번에 보여줘야 하기 때문에 false 로 설정.
         viewPager.setCurrentItem(adapter.itemCount / 2, false)
+
     }
 
+    // json 형식 파싱.
     private fun parseQuotesJson(json: String): List<Quote> {
         val jsonArray = JSONArray(json) //JSONObject 로 구성 되어있는 배열
         var jsonList = emptyList<JSONObject>()
+
+        // JSONArray 자체에서 foreach 등의 기능 제공하지 않으므로 아래와 같은 형태로 배열 생성 하였음.
         for (index in 0 until jsonArray.length()) {
             val jsonObject = jsonArray.getJSONObject(index)
-            // null 이 아니면
+            // null 이 아니면 리스트에 추가
             jsonObject?.let {
                 jsonList = jsonList + it
             }
         }
 
         // Quote 리스트로 변환
+        // map 을 사용하여 각 개체(JSONObject) 마다 Quote로 변환하여 Quote 리스트로 만듦.
         return jsonList.map {
             Quote(
                 it.getString("quote"),
--- app/src/main/java/lilcode/aop/p3/c02/todays_quote/QuotesPagerAdapter.kt
+++ app/src/main/java/lilcode/aop/p3/c02/todays_quote/QuotesPagerAdapter.kt
@@ -16,9 +16,10 @@ getItemCount()	전체 아이템 갯수 리턴.
 class QuotesPagerAdapter(
     private val quotes: List<Quote>,
     private val isNameRevealed: Boolean
-) :
+) : // 리사이클러뷰 어뎁터를 상속받으며 제너릭으로 뷰홀더를 설정해
     RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {
 
+    // 뷰 홀더로 구성해둔 레이아웃 설정(인플레이트)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder =
         QuoteViewHolder(
             LayoutInflater.from(parent.context)
@@ -26,26 +27,26 @@ class QuotesPagerAdapter(
         )
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        val actualPosition = position % quotes.size
+        val actualPosition = position % quotes.size // 무한 스크롤을 할 것 이므로 사이즈로 나눈 나머지로 위치 정함.
         holder.bind(quotes[actualPosition], isNameRevealed)
     }
 
     override fun getItemCount(): Int = Int.MAX_VALUE //quotes.size
 
-    // 뷰 홀더
+    // 뷰 홀더 클래스 (내부에 정의줌 해주었음)
     class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        // itemView : 우리가 정의 해준 레이아웃 뷰.
         private val quoteTextView: TextView = itemView.findViewById(R.id.quoteTextView)
         private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
 
         @SuppressLint("SetTextI18n")
         fun bind(quote: Quote, isNameRevealed: Boolean) {
             // 어떻게 랜더링 할 것인가
-            quoteTextView.text = "\"${quote.quote}\"" // 명언 내용
+            quoteTextView.text = "\"${quote.quote}\"" // 명언 내용 (큰따옴표 추가)
 
-            // 원격 isNameRevealed 에 따라 분기
+            // 원격 isNameRevealed 에 따라 분기 (Firebase 에서 설정된 불값을 가져와서 처리)
             if (isNameRevealed) {
-                nameTextView.text = "- ${quote.name}" // 작가
+                nameTextView.text = "- ${quote.name}" // 작가 (작대기 추가)
                 nameTextView.visibility = View.VISIBLE
             } else {
                 nameTextView.visibility = View.GONE
